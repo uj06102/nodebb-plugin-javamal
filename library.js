8564258390a6ba839558b433ddf26ef55aca0413
uj06102 <uj06102@naver.com>
2014-10-26 22:07:09 +0900

    Update library.js

--- library.js
+++ library.js
@@ -1,5 +1,5 @@
 module.exports.parse = function javamal(postContent, callback) {
-    postContent = postContent.replace(/<tr><td><a href="javascript:hideshow('javamal')">자바말<\/a><\/td><\/tr>/g, '<iframe width="1400" height="600" src="http://hanaone.com/turtle3d"></iframe>
+    postContent = postContent.replace(/<tr><td><a href="javascript:hideshow('javamal')">자바말<\/a><\/td><\/tr>/g, '<iframe width="1400" height="600" src="http://hanaone.com/turtle3d"></iframe>');
     callback(null, postContent);
 };
 
