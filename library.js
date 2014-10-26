2fc2ff8423203a01f2060f74797768e560323c3a
wonwooky <wonwooky@gmail.com>
2014-10-26 20:19:52 +0900

    just a base

--- library.js
+++ library.js
@@ -1,4 +1,10 @@
-module.exports.parse = function() {
-    document.location.href="http://hanaone.com/turtle3d";
+module.exports.parse = function(postContent, callback) {
+    postContent = postContent.replace(/<a href="(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)<\/a>/g, '<iframe class="youtube-plugin" width="640" height="360" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>');
+    callback(null, postContent);
 };
 
+function fold(code, index, isFolded){
+    if(isFolded){
+        
+    }
+}
